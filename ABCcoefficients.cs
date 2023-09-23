ff1955f60d5d86e29f48d58fccf052e8119ffdf2
Igor Shkredov <igorshkredov24@gmail.com>
2023-09-23 17:30:14 +0300

    Add project files.

---
+++ ABCcoefficients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProfHomeWork
+{
+    internal class ABCcoefficients
+    {
+        public int A { get;}
+        public int B { get;}
+        public int C { get;}
+
+        public ABCcoefficients(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using EquitationSolver;
+using System.Xml;
+using System.Text;
+
+namespace CSProfHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ABCcoefficients[] tasks = { new ABCcoefficients(2,8,2),
+                                         new ABCcoefficients(2,3,4),
+                                         new ABCcoefficients(3,4,0),
+                                         new ABCcoefficients(4,5,6),
+                                         new ABCcoefficients(2,4,2),
+                                         new ABCcoefficients(6,7,8),
+                                         new ABCcoefficients(7,8,9),
+                                         new ABCcoefficients(8,9,10),
+                                         new ABCcoefficients(9,10,11),
+                                         new ABCcoefficients(10,11,12),
+                                        };
+            List <Result> results = new List <Result> ();
+            
+            foreach (var task in tasks)
+            {
+                results.Add( eSolver.Resolve(task.A, task.B, task.C));
+            }
+
+            
+           Console.ReadLine ();
+        }
+    }
+}
